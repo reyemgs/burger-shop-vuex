a75e99b9f4baa85b343920cf29f82b2664fb0dfa
reyemgs <thetortyan@gmail.com>
2020-12-27 16:36:32 +0300

    feat: appear loader when load json

---
+++ src/components/Loader/Loader.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="loader-wrapper">
+    <div class="loader">
+      <div class="lds-dual-ring"></div>
+    </div>
+  </div>
+</template>
+
+<script src="./Loader.js" />
+
+<style lang="scss" src="./loader.scss" scoped />
